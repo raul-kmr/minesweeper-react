577d7a03e2448c0343f7fad9a2077502196ea586
rahul <rahulcs@hotmail.com>
2020-08-29 16:53:19 +0530

    setting up initial project files

---
+++ src/component/Cell.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Cell extends React.Component {
+  getValue() {
+    const { value } = this.props;
+
+    if (!value.isRevealed) {
+      return this.props.value.isFlagged ? "🚩" : null;
+    }
+    if (value.isMine) {
+      return "💣";
+    }
+    if (value.neighbour === 0) {
+      return null;
+    }
+    return value.neighbour;
+  }
+
+  render() {
+    const { value, onClick, cMenu } = this.props;
+    let className =
+      "cell" +
+      (value.isRevealed ? "" : " hidden") +
+      (value.isMine ? " is-mine" : "") +
+      (value.isFlagged ? " is-flag" : "");
+
+    return (
+      <div
+        onClick={onClick}
+        className={className}
+        onContextMenu={cMenu}
+      >
+        {this.getValue()}
+      </div>
+    );
+  }
+}
+
+const cellItemShape = {
+    isRevealed: PropTypes.bool,
+    isMine: PropTypes.bool,
+    isFlagged: PropTypes.bool
+}
+
+Cell.propTypes = {
+  value: PropTypes.objectOf(PropTypes.shape(cellItemShape)),
+  onClick: PropTypes.func,
+  cMenu: PropTypes.func
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Board from './component/Board';
+import './style.css';
+
+class Game extends React.Component {
+  state = {
+    height: 8,
+    width: 8,
+    mines: 10
+  };
+
+  render() {
+    const { height, width, mines } = this.state;
+    return (
+      <div className="game">
+        <Board height={height} width={width} mines={mines} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
